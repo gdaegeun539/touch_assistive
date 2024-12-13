7a0e552410446f01dc2b435a2cd75eed00119f62
Daegeun Lee <41503781+gdaegeun539@users.noreply.github.com>
2024-12-13 14:21:24 +0900

    내가 수정한대로 일단 엎어치기, 커밋 메시지는 나중에 다시 쓰기

--- lib/touch_assistive.dart
+++ lib/touch_assistive.dart
@@ -3,12 +3,13 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
+// 어세시티브 터치를 응용한 버튼은 아래 레포지토리의 코드를 응용했습니다.
+// https://github.com/PJRadadiya/touch_assistive
+
 class TouchAssistive extends StatefulWidget {
   final Widget child;
   final Offset initialOffset;
   final VoidCallback onPressed;
-  final EdgeInsets margin;
-  final bool shouldStickToSide;
   final double buttonSize;
   final double disableOpacity;
 
@@ -18,8 +19,6 @@ class TouchAssistive extends StatefulWidget {
     required this.initialOffset,
     required this.onPressed,
     required this.buttonSize,
-    this.margin = const EdgeInsets.all(8.0),
-    this.shouldStickToSide = true,
     this.disableOpacity = 0.5,
   });
 
@@ -32,8 +31,11 @@ class _TouchAssistiveState extends State<TouchAssistive> {
   late Offset _offset;
   late Offset largerOffset = _offset;
   bool isIdle = true;
-  Size size = Size.zero;
+  // Size size = Size.zero;
   Timer? timer;
+
+  double? lastOffsetX;
+  double? lastOffsetY;
 
   @override
   void initState() {
@@ -52,6 +54,7 @@ class _TouchAssistiveState extends State<TouchAssistive> {
 
   @override
   Widget build(BuildContext context) {
+    // FIXME: 빌드가 계속 일어나는 문제가 있다.
     return Positioned(
       left: _offset.dx,
       top: _offset.dy,
@@ -60,8 +63,8 @@ class _TouchAssistiveState extends State<TouchAssistive> {
         child: Listener(
           onPointerMove: (PointerMoveEvent pointerMoveEvent) {
             _updatePosition(pointerMoveEvent);
-
             setState(() {
+              isIdle = false;
               _isDragging = true;
             });
             _scheduleIdle();
@@ -69,6 +72,11 @@ class _TouchAssistiveState extends State<TouchAssistive> {
             _setOffset(_offset);
           },
           onPointerUp: (PointerUpEvent pointerUpEvent) {
+            bool isOffsetInCalibrationRange = _checkOffsetInCalibrationRange();
+            if (isOffsetInCalibrationRange) {
+              _isDragging = false;
+            }
+
             if (_isDragging) {
               setState(() {
                 _isDragging = false;
@@ -88,8 +96,12 @@ class _TouchAssistiveState extends State<TouchAssistive> {
 
   void _scheduleIdle() {
     timer?.cancel();
-    timer = Timer(const Duration(seconds: 1), () {
+    // MARK: 샘플링 레이트 1000ms -> 500ms로 변경
+    timer = Timer(const Duration(milliseconds: 500), () {
       if (_isDragging == false) {
+        // 멈춘 후에는 마지막 오프셋을 업데이트한다.
+        lastOffsetX = _offset.dx;
+        lastOffsetY = _offset.dy;
         setState(() {
           isIdle = true;
         });
@@ -109,15 +121,16 @@ class _TouchAssistiveState extends State<TouchAssistive> {
       return;
     }
 
-    final width = MediaQuery.of(context).size.width;
-    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.sizeOf(context).width;
+    final height = MediaQuery.sizeOf(context).height;
     const double left = 0;
     const double top = 0;
     final right = width - widget.buttonSize;
-    final bottom = height - (widget.buttonSize*3);
+    final bottom = height - (widget.buttonSize * 3);
     final middlePoint = width / 2;
 
     final topValue = max(min(offset.dy, bottom), top);
+    // 가장자리에 버튼이 붙는 것을 원할 경우 아래 코드를 사용
     final leftValue = max(
       min(
         topValue == bottom || topValue == top
@@ -129,8 +142,19 @@ class _TouchAssistiveState extends State<TouchAssistive> {
       ),
       left,
     );
+    // 버튼이 움직인 위치에 남는 것을 원할 경우 아래 코드를 사용
+    // final leftValue = max(min(offset.dx, right), left);
     setState(() {
       _offset = Offset(leftValue, topValue);
     });
+  }
+
+  /// 이전에 마지막으로 움직인 값과 지금 움직인 값이 오차범위(10픽셀) 내에 있는지 판단한다.
+  ///
+  /// 안드로이드의 경우 엣지로 인해 버튼을 누르는 것을 원했는데 움직이는 경우가 있어 이를 보정하기 위해 사용한다.
+  bool _checkOffsetInCalibrationRange() {
+    final isMoveXInCalibrationRange = lastOffsetX != null && (_offset.dx - lastOffsetX!) <= 10;
+    final isMoveYInCalibrationRange = lastOffsetY != null && (_offset.dy - lastOffsetY!) <= 10;
+    return isMoveXInCalibrationRange && isMoveYInCalibrationRange;
   }
 }
